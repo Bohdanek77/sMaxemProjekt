efa20b7147be7851c7c502df4425af7ac4e9c52c
spacimirr <spaca29@seznam.cz>
2025-10-09 13:13:17 +0200

    .

---
+++ MyMath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sMaxemProjekt
+{
+    internal class MyMath
+    {
+        public float Add(float a, float b)
+        {
+            return a + b;
+        }
+        public float Substract(float a, float b)
+        {
+            return a - b;
+        }
+        public float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+        public float Divide(float a, float b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+       
+    }
+}
