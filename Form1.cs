98b6e47f7530be37cc9ed75bbd06533103fe99c6
spacimirr <spaca29@seznam.cz>
2025-10-09 13:24:40 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ namespace sMaxemProjekt
 
         private void label1_Click(object sender, EventArgs e)
         {
-            label2.Text = "Zdravíme Gerhyho!";
+            
         }
 
         private void label2_Click(object sender, EventArgs e)
